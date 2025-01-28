21b74d66408e68dcb5a3c6c3d029e409da6a8ed1
Kezia <roselinakeziarijadi@gmail.com>
2025-01-28 02:08:09 -0800

    fix: change jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'], 
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+};
