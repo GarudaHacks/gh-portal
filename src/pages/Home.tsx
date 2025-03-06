38c27a079525bad4c8dc0febb75a481a5887df50
Kezia <roselinakeziarijadi@gmail.com>
2025-03-05 16:03:14 -0800

    merge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 // @ts-ignore
 import { useAuth } from "../context/AuthContext";
 import PageHeader from "../components/PageHeader";
+
 function Home() {
 	const user = useAuth();
 
