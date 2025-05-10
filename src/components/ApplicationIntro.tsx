a9894e7558adc17e95d97602eed82bf1e11fe7c4
Kezia <roselinakeziarijadi@gmail.com>
2025-05-09 23:44:32 -0700

    Updated events to reflect draft schedule

--- src/components/ApplicationIntro.tsx
+++ src/components/ApplicationIntro.tsx
@@ -11,12 +11,12 @@ export default function ApplicationIntro({
 }: ApplicationIntroProps) {
   return (
     <div className="p-4 flex flex-col items-center gap-4 lg:gap-6 w-full">
-      <RedGradientBackground className="w-full py-4 px-6 rounded-2xl flex flex-col gap-4 text-white shadow-md">
-        <h1 className="text-2xl font-bold">We're glad you're here.</h1>
+      <RedGradientBackground className="w-full p-6 rounded-2xl flex flex-col gap-4 text-white shadow-md">
+        <h1 className="text-xl font-bold">We're glad you're here.</h1>
         <p>{applicationIntro}</p>
         <div>
-          <p>Date:</p>
-          <p>Venue:</p>
+          <p>Date: <b>24 - 26 July 2025</b></p>
+          <p>Venue: <b>Universitas Multimedia Nusantara</b></p>
         </div>
       </RedGradientBackground>
       <Button className="w-full lg:w-fit place-self-end font-semibold" size="lg" onClick={onNextClick}>
