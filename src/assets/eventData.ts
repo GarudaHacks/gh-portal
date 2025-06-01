9678bcd016275e7fae0b56fc469cddc6a3d2e04e
Kezia <roselinakeziarijadi@gmail.com>
2025-05-31 20:51:36 -0400

    feat: integrated portal config

---
+++ src/assets/eventData.ts
@@ -0,0 +1,9 @@
+// TICKET
+const ticketDescription =
+  "This is your ticket for the event and will be needed to register (a.k.a. check in) at Garuda Hacks 2024. You will need to check in at least twice out of the three 12 hour intervals throughout the event. Please press 'View my ticket', then use the buttons below to add this ticket to your mobile wallet or take a screenshot of the ticket page. \n\n Registration begins at 5 PM local time in the Lobby of Universitas Multimedia Nusantara Main Building.";
+
+// APPLICATION
+const applicationIntro =
+  "Garuda Hacks wouldn't be where it is today without the passion, creativity, and trust of our participants. Since 2020, we've been humbled to host over 8,200 hackers and disburse more than 800 million IDR in prizes, and every year, we're blown away by the effort and heart our community brings.\n\nThis July, we're excited to return with our largest event yet: three days of building, learning, and connecting, with 100 million IDR in prizes, beginner-friendly workshops, live streams, and an inspiring lineup of speakers and judges from across the world. Our team reads each submission with care. We don't look at credentials, but at the thought, effort, and excitement you bring. \n\n Thank you for being here. We can't wait to learn more about you.";
+
+export { ticketDescription, applicationIntro };
