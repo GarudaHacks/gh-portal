5b32d705b3d2ac9ada0b3d16ef6630cb5dd022f4
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-21 17:14:50 +0700

    add react countdown

--- src/components/MentorshipAppointmentCard.tsx
+++ src/components/MentorshipAppointmentCard.tsx
@@ -3,6 +3,8 @@ import { epochToStringDate } from "@/utils/dateUtils"
 import { Badge } from "./ui/badge"
 import { ChevronDown, Lightbulb, MapPinCheck, MonitorSmartphone, MoreHorizontalIcon, Video } from "lucide-react"
 import { Alert, AlertDescription } from "@/components/ui/alert"
+import { titleCase } from "title-case";
+import Countdown, { zeroPad } from 'react-countdown';
 import {
   Collapsible,
   CollapsibleContent,
@@ -22,13 +24,23 @@ interface MentorshipAppointmentCardComponentProps {
   mentorshipAppointment: MentorshipAppointmentResponseAsHacker
 }
 
+const Completionist = () => <span>Mentoring started</span>;
+
+const renderer = ({ hours, minutes, seconds, completed }: { hours: number, minutes: number, seconds: number, completed: boolean }) => {
+  if (completed) {
+    return <Completionist />;
+  } else {
+    return <span>{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}</span>;
+  }
+};
+
 export default function MentorshipAppointmentCardComponent(
   { mentorshipAppointment }: MentorshipAppointmentCardComponentProps
 ) {
   return (
     <div className="border bg-blue-950/50 hover:bg-zinc-200/10 rounded-xl p-4 flex flex-col gap-4 h-fit">
-      <div className="flex flex-row gap-4 items-center justify-between ">
-        <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-6 gap-4 items-center justify-between">
+        <div className="col-span-5 flex flex-col gap-4">
           <div className="flex justify-between items-center">
             <div className='flex flex-row items-center gap-2'>
               <Badge variant={"default"} className="bg-green-500">Booked</Badge>
@@ -47,11 +59,11 @@ export default function MentorshipAppointmentCardComponent(
             {epochToStringDate(mentorshipAppointment.endTime)}{' '}
           </div>
 
-          <p>{(mentorshipAppointment.endTime - mentorshipAppointment.startTime) / 60}{' '}minutes</p>
+          <div>In <Countdown date={mentorshipAppointment.startTime * 1000} renderer={renderer} /></div>
           <p className="line-clamp-3 text-xs">{mentorshipAppointment.hackerDescription}</p>
         </div>
 
-        <div className="flex flex-col gap-2 items-center text-sm">
+        <div className="col-span-1 flex flex-col gap-2 items-center text-sm">
           <Button variant={"outline"} className="w-full"><img src="/images/icons/zoom-icon.svg" width={32} height={32} className="h-6" /></Button>
           <Button variant={"outline"} className="w-full"><img src="/images/icons/discord-icon.svg" width={32} height={32} className="h-6" /></Button>
           <Dialog>
@@ -62,7 +74,7 @@ export default function MentorshipAppointmentCardComponent(
               <DialogHeader>
                 <DialogTitle>Mentorship Detail</DialogTitle>
                 <DialogDescription className="text-white">
-              You can cancel your mentorship session up to 30 minutes before the appointment.
+                  You can cancel your mentorship session up to 45 minutes before the appointment.
                 </DialogDescription>
               </DialogHeader>
               <Button variant={"destructive"} className="">Cancel Mentorship</Button>
@@ -74,16 +86,23 @@ export default function MentorshipAppointmentCardComponent(
       <Collapsible>
         <CollapsibleTrigger className="flex flex-row gap-1 text-md justify-end w-full">
           <Badge variant={"outline"} className="text-white border-0">
-            <Lightbulb /> Instruction
+            <Lightbulb /> {titleCase(mentorshipAppointment.location)} Instruction
           </Badge>
         </CollapsibleTrigger>
         <CollapsibleContent className="py-4">
           <Alert variant={"default"}>
-            <AlertDescription className="text-white">
-              <ol>
-                <li>1. Private message your mentor through Discord for preparation in-advance</li>
-                <li>2. You can always join the zoom link</li>
-              </ol>
+            <AlertDescription className="text-white text-sm">
+              {mentorshipAppointment.location === 'online' ? (
+                <ol className="list-decimal p-4">
+                  <li>Reach out to the mentor to confirm whether the mentorship will be conducted via Zoom or another communication platform, such as Discord.</li>
+                  <li>You can always use the Zoom link by joining the Breakout Room designated for your mentor.</li>
+                </ol>
+              ) : (
+                <ol className="list-decimal p-4">
+                  <li>Reach out to the mentor to confirm the location of your meeting.</li>
+                  <li>You may use the Zoom link instead and join the Breakout Room designated for your mentor. The decision is up to you and the mentor.</li>
+                </ol>
+              )}
             </AlertDescription>
           </Alert>
         </CollapsibleContent>
