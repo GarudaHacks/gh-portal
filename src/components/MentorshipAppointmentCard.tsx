f409538d6a4baa7ab96324b84bbaf9cbacfb80f6
Heryan Djaruma <heryandjaruma@gmail.com>
2025-07-21 12:43:29 +0700

    better ui

--- src/components/MentorshipAppointmentCard.tsx
+++ src/components/MentorshipAppointmentCard.tsx
@@ -1,7 +1,7 @@
 import { MentorshipAppointmentResponseAsHacker } from "@/types/mentorship"
 import { epochToStringDate } from "@/utils/dateUtils"
 import { Badge } from "./ui/badge"
-import { ChevronDown, Lightbulb, MapPinCheck, MonitorSmartphone, Video } from "lucide-react"
+import { ChevronDown, Lightbulb, MapPinCheck, MonitorSmartphone, MoreHorizontalIcon, Video } from "lucide-react"
 import { Alert, AlertDescription } from "@/components/ui/alert"
 import {
   Collapsible,
@@ -9,6 +9,14 @@ import {
   CollapsibleTrigger,
 } from "@/components/ui/collapsible"
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 interface MentorshipAppointmentCardComponentProps {
   mentorshipAppointment: MentorshipAppointmentResponseAsHacker
@@ -18,8 +26,8 @@ export default function MentorshipAppointmentCardComponent(
   { mentorshipAppointment }: MentorshipAppointmentCardComponentProps
 ) {
   return (
-    <div className="border rounded-xl p-4 flex flex-col gap-4 h-fit">
-      <div className="flex flex-row gap-4 items-baseline justify-between ">
+    <div className="border bg-blue-950/50 hover:bg-zinc-200/10 rounded-xl p-4 flex flex-col gap-4 h-fit">
+      <div className="flex flex-row gap-4 items-center justify-between ">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between items-center">
             <div className='flex flex-row items-center gap-2'>
@@ -40,18 +48,33 @@ export default function MentorshipAppointmentCardComponent(
           </div>
 
           <p>{(mentorshipAppointment.endTime - mentorshipAppointment.startTime) / 60}{' '}minutes</p>
-
+          <p className="line-clamp-3 text-xs">{mentorshipAppointment.hackerDescription}</p>
         </div>
 
-        <div className="flex flex-row gap-2 items-center text-sm">
-          <Button variant={"outline"} className="p-0"><Video /></Button>
+        <div className="flex flex-col gap-2 items-center text-sm">
+          <Button variant={"outline"} className="w-full"><img src="/images/icons/zoom-icon.svg" width={32} height={32} className="h-6" /></Button>
+          <Button variant={"outline"} className="w-full"><img src="/images/icons/discord-icon.svg" width={32} height={32} className="h-6" /></Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={"outline"} className="w-full"><MoreHorizontalIcon /></Button>
+            </DialogTrigger>
+            <DialogContent className="text-white">
+              <DialogHeader>
+                <DialogTitle>Mentorship Detail</DialogTitle>
+                <DialogDescription className="text-white">
+              You can cancel your mentorship session up to 30 minutes before the appointment.
+                </DialogDescription>
+              </DialogHeader>
+              <Button variant={"destructive"} className="">Cancel Mentorship</Button>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
 
       <Collapsible>
-        <CollapsibleTrigger className="flex flex-row gap-1 text-md">
+        <CollapsibleTrigger className="flex flex-row gap-1 text-md justify-end w-full">
           <Badge variant={"outline"} className="text-white border-0">
-            <Lightbulb /> Instruction <ChevronDown />
+            <Lightbulb /> Instruction
           </Badge>
         </CollapsibleTrigger>
         <CollapsibleContent className="py-4">
