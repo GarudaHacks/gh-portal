fb92a096ff50cf094e5f4abe76759f4d7c9f7f40
Heryan Djaruma <heryandjaruma@gmail.com>
2026-06-16 13:23:50 +0800

    better layouting for applications open soon, open, and submitted

--- src/pages/application/ApplicationSubmitted.tsx
+++ src/pages/application/ApplicationSubmitted.tsx
@@ -24,12 +24,12 @@ export default function ApplicationSubmitted({ portalConfig }: ApplicationSubmit
         </div>
         <img src={garudieReview} width={300} height={300} className="w-72 hidden lg:block" />
       </div>
-      <div className="w-full p-8 border border-tertiary grid grid-cols-1 lg:grid-cols-5 gap-10 bg-white rounded-xl border-tertiary-glow">
+      <div className="w-full p-8 border border-tertiary grid grid-cols-1 lg:grid-cols-5 place-items-center gap-10 bg-white rounded-xl border-tertiary-glow">
         <div className="hidden lg:block p-4 bg-tertiary/10 rounded-xl aspect-square">
           <img src={applicationReview} alt="Hourglass" width={300} height={300} className="" />
         </div>
 
-        <div className="col-span-full lg:col-span-3 flex flex-col gap-6 lg:gap-4">
+        <div className="col-span-full lg:col-span-3 flex flex-col gap-6 lg:gap-4 w-full">
           <div className="flex lg:flex-row items-center justify-center lg:justify-start gap-2 py-2 px-3 bg-tertiary/10 w-full lg:w-fit rounded-xl">
             <PartyPopper size={16} className="text-tertiary animate-bounce" />
             <span className="font-semibold text-tertiary">Application is submitted</span>
@@ -54,6 +54,7 @@ export default function ApplicationSubmitted({ portalConfig }: ApplicationSubmit
             <p className="flex flex-col lg:flex-row gap-2"><span className="font-semibold text-sm lg:text-base">Venue : </span> Universitas Multimedia Nusantara (UMN)</p>
           </div>
         </div>
+
         <div className="bg-tertiary/10 rounded-xl p-6 flex flex-col gap-4">
           <p className="font-bold text-tertiary">What happens next:</p>
           <div className="flex flex-col">
