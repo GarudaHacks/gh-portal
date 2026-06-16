7d9ecde248bd4479b5301f38e5e1d5b1139d5cc0
Heryan Djaruma <heryandjaruma@gmail.com>
2026-06-16 20:36:59 +0800

    remove notify me button in application open soon

--- src/pages/application/states/ApplicationOpenSoon.tsx
+++ src/pages/application/states/ApplicationOpenSoon.tsx
@@ -1,26 +1,19 @@
-import { Bell, Calendar, Clock, Hourglass, MapPin } from "lucide-react"
+import { Calendar, Clock, Hourglass, MapPin } from "lucide-react"
 import applicationHourglass from "/assets/application-hourglass.png"
 import { PortalConfig } from "@/utils/portalConfig"
-import { Button } from "@/components/ui/button"
-import { useNavigate } from "react-router-dom"
-import toast from "react-hot-toast"
 
 interface ApplicationOpenSoonProps {
   portalConfig: PortalConfig | null
 }
 
 export default function ApplicationOpenSoon({ portalConfig }: ApplicationOpenSoonProps) {
-  const handleNotifyMe = () => {
-    toast.success("Notification set!")
-    // TODO: implement this
-  }
   return (
     <div className="w-full p-8 border border-tertiary grid grid-cols-1 lg:grid-cols-5 gap-10 bg-white rounded-xl border-tertiary-glow">
       <div className="hidden lg:block p-4 bg-tertiary/10 rounded-xl aspect-square">
         <img src={applicationHourglass} alt="Hourglass" width={300} height={300} className="" />
       </div>
 
-      <div className="col-span-1 lg:col-span-3 flex flex-col gap-6 lg:gap-4">
+      <div className="col-span-1 lg:col-span-4 flex flex-col gap-6 lg:gap-4">
         <div className="flex lg:flex-row items-center justify-center lg:justify-start gap-2 py-2 px-3 bg-tertiary/10 w-full lg:w-fit rounded-xl">
           <Hourglass size={16} className="text-tertiary animate-pulse" />
           <span className="font-semibold text-tertiary">Applications opening soon</span>
@@ -44,19 +37,6 @@ export default function ApplicationOpenSoon({ portalConfig }: ApplicationOpenSoo
           </div>
           <p className="flex flex-col lg:flex-row gap-2"><span className="font-semibold text-sm lg:text-base">Venue : </span> Universitas Multimedia Nusantara (UMN)</p>
         </div>
-      </div>
-      <div className="text-center items-center flex flex-col gap-2">
-        <div className="flex-1"></div>
-        <Button
-          className={`w-full ${!portalConfig?.applicationsOpen ? "opacity-90 cursor-not-allowed" : ""
-            }`}
-          disabled={!portalConfig?.applicationsOpen}
-          onClick={handleNotifyMe}
-          size={"lg"}
-        >
-          Notify me <Bell />
-        </Button>
-        <p className="text-xs text-muted">We'll notify you by email</p>
       </div>
     </div>
   )
