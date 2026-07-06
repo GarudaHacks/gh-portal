615d20a1cb460a297a58e494a38c792e624fcdd4
RyanDJ <heryandjaruma@gmail.com>
2026-07-06 22:47:33 +0800

    add application closed state

--- src/pages/application/ApplyProcessCard.tsx
+++ src/pages/application/ApplyProcessCard.tsx
@@ -4,6 +4,7 @@ import garudieRocket from "/assets/garudie-rocket.png"
 import ApplicationOpen from "./states/ApplicationOpen";
 import ApplicationOpenSoon from "./states/ApplicationOpenSoon";
 import GarudieWelcome from "@/components/GarudieWelcome";
+import ApplicationClosed from "./states/ApplicationClosed";
 
 interface ApplyProcessCardProps {
   portalConfig: PortalConfig | null
@@ -16,7 +17,8 @@ export default function ApplyProcessCard({ portalConfig }: ApplyProcessCardProps
     const currentDate = new Date()
     if (user?.status === "submitted") return "submitted"
     if (portalConfig?.applicationStartDate! > new Date()) return "soon"
-    if (portalConfig?.applicationsOpen && portalConfig?.applicationStartDate! < currentDate) return "open"
+    if (portalConfig?.applicationsOpen && portalConfig?.applicationStartDate! < currentDate && portalConfig?.applicationCloseDate! > currentDate) return "open"
+    if (!portalConfig?.applicationsOpen || portalConfig?.applicationCloseDate! < currentDate) return "closed"
   }
 
   const currentProcess = getCurrentProcess()
@@ -26,6 +28,7 @@ export default function ApplyProcessCard({ portalConfig }: ApplyProcessCardProps
       <GarudieWelcome image={garudieRocket} imageTopPosition={4} imageRightPosition={0} imageSizing="w-24 lg:w-64" />
       {currentProcess === "soon" && <ApplicationOpenSoon portalConfig={portalConfig} />}
       {currentProcess === "open" && <ApplicationOpen portalConfig={portalConfig} />}
+      {currentProcess === "closed" && <ApplicationClosed />}
     </div>
   )
 }
